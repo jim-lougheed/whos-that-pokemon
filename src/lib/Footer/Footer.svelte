daba3dc9a7dbf6f79185e70bec87356029842081
Jim Lougheed <jims.codes@gmail.com>
2024-10-03 12:07:49 -0400

    Add Main page and Footer

---
+++ src/lib/Footer/Footer.svelte
@@ -0,0 +1,23 @@
+<div class="footer-container">
+    <p>© 
+        <a href="https://jim-lougheed.github.io/portfolio/">
+            JimsCodes
+        </a>
+        {new Date().getFullYear()}{" "}
+    </p>
+</div>
+
+<style>
+    .footer-container {
+        background: black;
+        width: 100px;
+        padding: 20px 0;
+        width: 100%;
+        position: absolute;
+        bottom: 0;
+    }
+    .footer-container p {
+        margin: 0 auto;
+        width: fit-content;
+    }
+</style>
---
+++ src/lib/Main/Main.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let score = 0;
+    const incrementScore = () => {
+        score += 1
+    }
+</script>
+
+<button on:click={incrementScore}>
+    Your score is {score}
+</button>
