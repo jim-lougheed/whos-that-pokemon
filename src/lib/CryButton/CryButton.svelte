02d83ca71a1c3c77386b6b22b90dd490a7b20d81
Jim <jims.codes@gmail.com>
2024-10-14 15:51:07 -0400

    Add cry button

---
+++ src/lib/CryButton/CryButton.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let cryNum: number;
+    const ctx = new AudioContext();
+    let soundFile: AudioBuffer;
+
+    fetch(`http://localhost:8080/cries/${cryNum}.ogg`)
+    .then(data => data.arrayBuffer())
+    .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
+    .then(decodedAudio => { 
+        soundFile = decodedAudio;
+    });
+
+    function playback() {
+        const playSound = ctx.createBufferSource();
+        playSound.buffer = soundFile;
+        playSound.connect(ctx.destination);
+        playSound.start(ctx.currentTime);
+    }
+</script>
+
+<button on:click={() => playback()}>
+    Play sound
+</button>
--- src/lib/Main/Main.svelte
+++ src/lib/Main/Main.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import Card from "../Card/Card.svelte";
 import { POKEMON_TOTAL } from "../../common/constants";
+  import CryButton from "../CryButton/CryButton.svelte";
 
     let score = 0;
     
@@ -23,27 +24,6 @@ import { POKEMON_TOTAL } from "../../common/constants";
     }
 
     const randomPokemonNumbers = getRandomPokemonNumbers(3);
-
-    const ctx = new AudioContext();
-    let soundFile: AudioBuffer[] = [];
-
-    for (let i = 0; i < 10; i++) {
-        fetch(`http://localhost:8080/cries/${i + 1}.ogg`)
-        .then(data => data.arrayBuffer())
-        .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
-        .then(decodedAudio => { 
-            console.log("HERE 2");
-            soundFile[i] = decodedAudio;
-        });
-    }
-
-    function playback(index: number) {
-        console.log("PLAY BACKING", index);
-        const playSound = ctx.createBufferSource();
-        playSound.buffer = soundFile[index - 1];
-        playSound.connect(ctx.destination);
-        playSound.start(ctx.currentTime);
-    }
 </script>
 
 <div class="score-container">
@@ -54,6 +34,10 @@ import { POKEMON_TOTAL } from "../../common/constants";
     <Card cardIndex={1} cardNum={randomPokemonNumbers[0]}/>
     <Card cardIndex={2} cardNum={randomPokemonNumbers[1]}/>
     <Card cardIndex={3} cardNum={randomPokemonNumbers[2]}/>
+</div>
+
+<div class="cry-button-container">
+    <CryButton cryNum={randomPokemonNumbers[0]}/>
 </div>
 
 <style>
